23acd73e8c2defaccb0262fe74cbbf4606a7ab89
Boene <abushibika@gmx.de>
2015-02-07 15:56:26 +0100

    First basic PHP version of the list class

---
+++ lesson3/List.php
@@ -0,0 +1,44 @@
+    <?php
+		class Lists{ #this is the class which defines the structure, attributes and functions of created ToDoLists
+			public function __construct(){ #defines the basic state of a new created list (public?)
+				
+			}
+			
+			public function __destruct(){ #defines the way a list is deleted (public?)
+				
+			}
+			
+			public function setName(){ #give the list a name
+				
+			}
+			
+			public function getName(){ #get the lists name
+				
+			}
+			
+			public function setNewItem(){ #add a new item to the list
+				
+			}
+			
+			public function getItems(){ #get all the items from a list
+				
+			}
+			
+			public function changeItem(){ #change any attribute of an item (or will that be part of the item class itself?)
+				
+			}
+			
+			public function deleteItem(){ #delete an item
+				
+			}
+			
+			public function addUser(){ #add access to the list for an additional user
+				
+			}
+			
+			public function deleteUser(){ #delete access to the list for a user that is not the creator of the list (? should creator be deletable?))
+				
+			}
+			
+		}
+	?>
