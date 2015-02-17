e3d95ae80d55e4936d1c0c89819548d744481c7c
Boene <abushibika@gmx.de>
2015-02-17 16:25:35 +0100

    3 new Files
    Created these in the style used before for List.php (ToDoList.php).
    Change from List.php -> ToDoList.php not realized to this point.

---
+++ src/Item.php
@@ -0,0 +1,38 @@
+/**
+* @file Item.php
+*
+* @brief Contains the class "Item" and defines its structure and behaviour.
+*/
+
+<?php
+
+class Item {
+	
+	public function __construct ($task, ...$detail, $progress) {
+		
+	}
+	
+	public function setTitle ($title) {
+		
+	}
+	
+	public function getTitle () {
+		
+	}
+	
+	public function setDetail ($detail) {
+		
+	}
+	
+	public function getDetail () {
+		
+	}
+	
+	public function setProgress ($progress) {
+		
+	}
+	
+	public function getProgress () {
+		
+	}
+}
---
+++ src/User.php
@@ -0,0 +1,38 @@
+/**
+* @file User.php
+*
+* @brief Contains the class "User" and defines its structure and behaviour.
+*/
+
+<?php
+
+class User {
+	
+	public function __construct ($username, $password, $email) # wie verfahren wir an der Stelle mit Verschlüsselung für Mail und Passwort? Und wie realisiert sich der Unterschied zwischen neuem Account anlegen und zB einloggen?
+	
+	}
+	
+	public function getUsername () {
+		
+	}
+	
+	public function changeEmail ($email) {
+		
+	}
+	
+	public function getEmail ($email) { # getPassword wirkt auf mich fehl am Platz, nicht sicher obs bei mail das gleiche is
+		
+	}
+	
+	public function forgotEmail () {
+		
+	}
+	
+	public function forgotPassword () {
+		
+	}
+	
+	public function changePassword ($password) {
+		
+	}
+?>	
