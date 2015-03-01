367f4ed3a11dd3e66c4167da8551dae1b8c368b1
Boene <abushibika@gmx.de>
2015-03-01 13:28:28 +0100

    Added first version of interface 'ToDoListStorage' using a CRUD attempt.

---
+++ src/ToDoListStorage.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * @file ToDoListStorage.php
+ *
+ * @brief Contains the interface "ToDoListInterface" which defines a set of 
+ * functions demanded to be defined for every ToDoList. These functions 
+ * represent a CRUD approach.
+ */
+
+/**
+ * @interface ToDoListStorage 
+ *
+ * @brief This Interface is implemented by the "ToDoList" class.
+ *
+ */
+interface ToDoListInterface {
+    public function listTasks() { // wirkt auf mich als könnte es nützlich sein
+        
+    }
+    
+    public function createList() {
+        
+    }
+    
+    public function openList() {
+        
+    }
+    
+    public function deleteList() {
+        
+    }
+    
+    public function updateList() {
+        
+    }
+}
+?>
