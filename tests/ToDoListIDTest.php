63977036e705632308003696ff5657c245c76041
Boene <abushibika@gmx.de>
2015-03-29 14:35:21 +0200

    forgot to add ToDoListIDTest.php in previous commit

---
+++ tests/ToDoListIDTest.php
@@ -0,0 +1,11 @@
+<?php
+require_once(/../src/ToDoList.php); 
+
+class ToDoListIDTest extends PHPUnit_Framework_TestCase {
+    public function testCanUseToDoListID() {
+        $listID = new ToDoListID(345);
+        
+        $this->assertEquals(345, $listID->value());
+    }
+}    
+?>
